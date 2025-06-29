2a0dc1f549a2419d5bf90db8419a17cfafa6942b
Walid-shehata <64784318+Walid-shehata@users.noreply.github.com>
2025-06-29 15:34:06 +0400

    Update startup.sh

--- dockerfiles/startup.sh
+++ dockerfiles/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo '[ -f /etc/profile ] && source /etc/profile' >> ~/.bashrc
+echo '[ -f ~/.bash_profile ] && source ~/.bash_profile' >> ~/.bashrc
+
 /usr/local/bin/inject_keys.sh
 
 echo "Welcome to $(hostname)"
